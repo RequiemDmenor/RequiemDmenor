2f65224433c215316aa27f830b9e66e64eb8c2d9
Rodrigo <rml010203@gmail.com>
2022-02-19 14:59:47 +0100

    PRUEBA

---
+++ hello-world/src/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 3, 2022
+ *      Author: atcsol
+ */
+
+#include <stdio.h>
+int main () {
+	char hola [255];
+	printf ("Buenos días\n");
+	scanf ("%s",hola);
+	printf ("Me caes mal tonto");
+
+	char adios [255];
+	printf ("Buenos días\n");
+	scanf ("%c", adios);
+	printf ("Que pase una buena tarde");
+
+	return 0;}
